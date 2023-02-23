4005af0bc7de53a6b43a01b4d400c7e482c8240e
Kevin Bell <bellKevin@pm.me>
2023-02-22 20:26:56 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -23,13 +23,15 @@ export default function App() {
         <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) =>
-          <View style={styles.goalItem} key={index}>
-            <Text style={styles.goalText}>
-              {goal}
-            </Text>
-          </View>
-        )}
+        <ScrollView>
+          {courseGoals.map((goal, index) =>
+            <View style={styles.goalItem} key={index}>
+              <Text style={styles.goalText}>
+                {goal}
+              </Text>
+            </View>
+          )}
+        </ScrollView>
       </View>
     </View>
   );
